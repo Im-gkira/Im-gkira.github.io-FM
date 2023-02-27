be6626c0cb17fe5d6c073c028fbf46dadb28bd7b
Im-gkira <gurkirat.11912048@iiitsonepat.ac.in>
2023-02-28 01:07:42 +0530

    Frontend mentors qr challange first commit

---
+++ src/components/card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Card(){
+    return (
+        <div className="main">
+            <img className="qr--code" src="images/image-qr-code.png"/>
+            <div className="content">
+                <h3>Improve your front-end skills by building projects</h3>
+                <p>scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+            </div>
+        </div>
+    )
+}
