82f216c422732b2221285e3009c799b62ea2ea69
Im-gkira <gurkirat.11912048@iiitsonepat.ac.in>
2023-02-28 01:27:42 +0530

    added alt text to image

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Card(){
     return (
         <div className="main">
-            <img className="qr--code" src="images/image-qr-code.png"/>
+            <img className="qr--code" src="images/image-qr-code.png" alt="QR Code to scan"/>
             <div className="content">
                 <h3>Improve your front-end skills by building projects</h3>
                 <p>scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
